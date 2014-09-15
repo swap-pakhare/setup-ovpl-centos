94d48eeb6ea49733212ead8badbe877e177a9e12
Anon Ray <rayanon004@gmail.com>
2014-09-15 21:33:24 +0530

    Add corkscrew installation
      - Add corkscrew installation and update ssh config to use corkscrew for
        tunneling ssh over HTTP

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 meta_dir="./meta"
+ssh_file="~/.ssh/config"
 
 echo "Setting up EPEL repo.."
 #wget http://download.fedoraproject.org/pub/epel/6/i386/epel-release-6-8.noarch.rpm
@@ -11,5 +12,19 @@ rpm -ivh $meta_dir/epel-release-6-8.noarch.rpm
 
 echo "Installing dependencies.."
 yum -y install gcc python-devel.x86_64 python-pip git
+
+echo "Setting up ssh over HTTP.."
+cd $meta_dir/corkscrew-2.0
+echo "Installing corkscrew.."
+./configure
+make
+make install
+
+path_to_corkscrew=$(which corkscrew)
+
+echo "Updating ssh config file.."
+echo "" >> $ssh_file
+echo "host github.com" >> $ssh_file
+echo "ProxyCommand $path_to_corkscrew proxy.iiit.ac.in 8080 %h %p" >> $ssh_file
 
 exit 0
