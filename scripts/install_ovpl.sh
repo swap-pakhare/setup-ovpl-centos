292221bc9e23e8d4dd1e4e26a0caa618fad126f4
ksripathi <kammari.sripathi@gmail.com>
2014-11-20 10:21:54 +0530

    Delete install_ovpl.sh

--- scripts/install_ovpl.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo ""
-echo "Congrats! You have setup OVPL successfully!!! :-)"
-echo "Now run 'make' from inside the src directory"
-echo " of OVPL to start the services."
-
-exit 0
