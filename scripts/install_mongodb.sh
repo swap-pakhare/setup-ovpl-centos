7770d1577d06450397e00678101e6006afbfa454
ksripathi <kammari.sripathi@gmail.com>
2014-11-20 10:21:38 +0530

    Update install_mongodb.sh

--- scripts/install_mongodb.sh
+++ scripts/install_mongodb.sh
@@ -15,5 +15,9 @@ yum -y install mongodb-org
 echo ""
 echo "Starting mongod service.."
 service mongod start
+echo ""
+echo "Congrats! You have setup OVPL successfully!!! :-)"
+echo "Now run 'make' from inside the src directory"
+echo " of OVPL to start the services."
 
 exit 0
