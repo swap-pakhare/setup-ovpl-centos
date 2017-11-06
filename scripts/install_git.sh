883bf9ee43636da5872ad04828500d172c60a3c8
ksripathi <kammari.sripathi@gmail.com>
2017-11-06 17:39:26 +0530

    added stepts to install git 2.7.4

--- scripts/install_git.sh
+++ scripts/install_git.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 # Script to install git
-# Installs: Dependencies: curl-devel expat-devel gettext-devel openssl-devel zlib-devel gcc perl-ExtUtils perl-devel
+# Installs: Dependencies: curl-devel expat-devel gettext-devel openssl-devel zlib-devel gcc perl-ExtUtils-MakeMaker
 
-yum erase -y git
-yum -y update
-yum install -y curl-devel expat-devel gettext-devel openssl-devel zlib-devel gcc perl-ExtUtils
-yum install -y perl-devel
+yum update -y
+
+yum install curl-devel expat-devel gettext-devel openssl-devel zlib-devel -y
+yum install  gcc perl-ExtUtils-MakeMaker -y
+
+yum remove git
 
 cd /usr/src
-echo "Get the git sources"
-wget https://www.kernel.org/pub/software/scm/git/git-2.0.1.tar.gz
-tar xzf git-2.0.1.tar.gz
-cd git-2.0.1
-echo "Install git from sources"
+
+wget https://www.kernel.org/pub/software/scm/git/git-2.7.4.tar.gz
+tar xzf git-2.7.4.tar.gz
+cd git-2.7.4
 make prefix=/usr/local/git all
 make prefix=/usr/local/git install
-echo "export PATH=$PATH:/usr/local/git/bin" >> /etc/bashrc
+
+echo 'export PATH=$PATH:/usr/local/git/bin' >> /etc/bashrc
 source /etc/bashrc
-echo $PATH
+
 
 exit 0
 
